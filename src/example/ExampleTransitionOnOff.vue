643c7f0c9982c008202cd2206a033ec03ac659d8
tink <tink@0block.com>
2022-03-11 15:51:04 +0800

    transition on | list

---
+++ src/example/ExampleTransitionOnOff.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="example-2">
+		<transition name="toggle">
+			<button key="on" @click="toggle" v-if="active">
+				on
+			</button>
+			<button key="off" @click="toggle" v-else>off</button>
+		</transition>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			active: true
+		}
+	},
+	methods: {
+		toggle() {
+			this.active = !this.active
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+#example-2 {
+	position: relative;
+	height: 40px;
+}
+button {
+	position: absolute;
+}
+.toggle-enter-active,
+.toggle-leave-active {
+	transition: all 0.5s linear;
+}
+.toggle-enter,
+.toggle-leave-to {
+	opacity: 0;
+}
+.toggle-enter {
+	transform: translateX(100%);
+}
+.toggle-leave-to {
+	transform: translateX(-100%);
+}
+</style>
--- src/example/index.vue
+++ src/example/index.vue
@@ -1,13 +1,21 @@
 <template>
 	<div id="app">
 		<!-- <example-prop-or-attr></example-prop-or-attr> -->
+		<hr>
 		<example-sync v-bind.sync="obj"></example-sync>
 		{{obj.a}}
 		{{obj.b}}
 		<div ref="testRef">testRef</div>
+		<hr>
 		<example-ref-cmp ref="refCmp"></example-ref-cmp>
+		<hr>
 		<example-once-hook v-if="show"></example-once-hook>
+		<hr>
 		<button @click="show=!show">toggle</button>
+		<hr>
+		<example-transition-on-off></example-transition-on-off>
+		<hr>
+		<example-transition-list></example-transition-list>
 	</div>
 </template>
 
